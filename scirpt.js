afe584e19c77a0ac358683e100752627f75d6b72
Ereklebazanovi <erekle.bazanovi.1@iliauni.edu.ge>
2024-09-12 00:57:37 +0400

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,13 @@
+function words(word1,word2){
+
+    if(word1.length === word2.length){
+        return true
+    }
+    else {
+        return false
+    }
+
+
+}
+
+console.log(words('სიტყვები', 'სიტყვები'))
